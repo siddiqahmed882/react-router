ede8bbd6abfa7f41b94deed4bf88c6426b3baf23
Siddiq <siddiqahmed882@gmail.com>
2021-10-15 01:57:27 +0500

    css fixes

--- src/Nav.js
+++ src/Nav.js
@@ -3,16 +3,18 @@ const Nav = ({ search, setSearch }) => {
   const path = window.location.pathname;
   return (
     <nav className='Nav'>
-      <form className='searchForm' onSubmit={(e) => e.preventDefault()}>
-        <label htmlFor='search'>Search Posts</label>
-        <input
-          id='search'
-          type='text'
-          placeholder='Search Posts'
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-        />
-      </form>
+      {path === '/' && (
+        <form className='searchForm' onSubmit={(e) => e.preventDefault()}>
+          <label htmlFor='search'>Search Posts</label>
+          <input
+            id='search'
+            type='text'
+            placeholder='Search Posts'
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </form>
+      )}
       <ul>
         <li className={path === '/' ? 'active' : null}>
           <Link to='/'>Home</Link>
